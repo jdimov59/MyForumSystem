7e49cf2b6d634fa7f82f6711ce2e979ee1c9b47a
jdimov59 <jdimov59@gmail.com>
2016-05-14 19:06:29 +0300

    Implement IAuditInfo &IDeletableEntity in ApplicationUser & ApplicationDbContext

--- Source/Data/MyForumSystem.Data/ApplicationDbContext.cs
+++ Source/Data/MyForumSystem.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MyForumSystem.Data.Common.Models;
 using MyForumSystem.Data.Migrations;
 using MyForumSystem.Data.Models;
 using System;
@@ -24,5 +25,51 @@ namespace MyForumSystem.Data
         }
 
         public IDbSet<Tag> Tags { get; set; }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    if (!entity.PreserveCreatedOn)
+                    {
+                        entity.CreatedOn = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
+
+        private void ApplyDeletableEntity()
+        {
+            //Approach via @julielerman:http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(e => e.Entity is IDeletableEntity && (e.State == EntityState.Deleted)))
+            {
+                var entity = (IDeletableEntity)entry.Entity;
+
+                entity.DeletedOn = DateTime.Now;
+                entity.IsDeleted = true;
+                entry.State = EntityState.Modified;
+            }
+        }
     }
 }
